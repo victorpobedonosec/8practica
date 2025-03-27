72db2de308cf6891e3b3329ae08839da2abb2a99
Кирилл <protskie@yandex.ru>
2025-03-27 11:12:36 +0300

    8practica

---
+++ app/src/main/java/ru/netology/nmedia/dto/Post.kt
@@ -0,0 +1,13 @@
+package ru.netology.nmedia.dto
+
+data class Post(
+    val id: Long,
+    val author: String,
+    val content: String,
+    val published: String,
+    var likedByMe: Boolean = false,
+    val likes: Int = 0,
+    var shares: Int = 1,
+
+)
+
